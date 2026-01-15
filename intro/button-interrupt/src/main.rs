aa78d2e405e0c5e53f3072d2f0eefc983e748263
kyouraku <139937096+kyouraku1024@users.noreply.github.com>
2026-01-15 20:07:14 +0800

    write botton-interrupt

--- intro/button-interrupt/src/main.rs
+++ intro/button-interrupt/src/main.rs
@@ -6,10 +6,14 @@ use critical_section::Mutex;
 use esp_backtrace as _;
 use esp_hal::{
     delay::Delay,
-    gpio::{Event, Input, InputConfig, Io, Level, Output, OutputConfig},
+    gpio::{Event, Input, InputConfig, Io},
+    rmt::Rmt,
+    time::Rate,
     handler, main,
 };
 use esp_println::println;
+use smart_leds::{SmartLedsWrite, RGB8};
+use esp_hal_smartled::{buffer_size, color_order, RmtSmartLeds, Sk68xxTiming};
 
 esp_bootloader_esp_idf::esp_app_desc!();
 
@@ -23,15 +27,35 @@ fn main() -> ! {
 
     let mut io = Io::new(peripherals.IO_MUX);
     // Set the interrupt handler for GPIO interrupts.
+    io.set_interrupt_handler(handler);
 
-    // Set GPIO7 as an output, and set its state high initially.
-    let mut led = Output::new(peripherals.GPIO7, Level::Low, OutputConfig::default());
+    // Set GPIO8 as an output, and set its state high initially.
+    let rmt = Rmt::new(peripherals.RMT, Rate::from_mhz(80)).unwrap();
+    // Create LED driver for SK68XXMINI-HS (1 LED)
+    let mut led = RmtSmartLeds::<{ buffer_size::<RGB8>(1) }, _, RGB8, color_order::Grb, Sk68xxTiming>::new(
+        rmt.channel0,
+        peripherals.GPIO8,
+    )
+    .unwrap();
+    // Purple color: Red + Blue (255, 0, 255)
+    let purple = RGB8::new(50, 0, 50);
+    let off = RGB8::new(0, 0, 0);
 
     // Set GPIO9 as an input
     let mut button = Input::new(peripherals.GPIO9, InputConfig::default());
 
+    critical_section::with(|cs| {
+        button.listen(Event::FallingEdge);
+        BUTTON.borrow_ref_mut(cs).replace(button);
+    });
+
     let delay = Delay::new();
-    loop {}
+    loop {
+        led.write([purple]).unwrap();
+        delay.delay_millis(500);
+        led.write([off]).unwrap();
+        delay.delay_millis(500);
+    }
 }
 
 #[handler]
